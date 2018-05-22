cec047abe932ffa9f348fedc9ffb8afc68e50194
eunmo <eunmo@cs.stanford.edu>
2018-05-23 00:56:40 +0900

    Initial Commit

---
+++ Front Page/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Front Page
+//
+//  Created by Eunmo Yang on 5/23/18.
+//  Copyright © 2018 Eunmo Yang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
